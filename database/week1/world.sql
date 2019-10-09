390e8ca668d279682db9f919e863f43ed1db3442
Suthatta Lindstrii <psuthatta@gmail.com>
2019-10-09 20:33:49 +0200

    update changed city table

--- database/week1/world.sql
+++ database/week1/world.sql
@@ -1,94 +1,200 @@
 create database world;
 use world;
-show databases;
-
+show tables;
 drop table country;
+drop table city;
 
 create table country
 (
   Country_Code varchar(3) NOT NULL DEFAULT '',
   Country_Name varchar(30) NOT NULL DEFAULT '',
-  Continent ENUM('Asia','Africa','Antarctica','Australia', 'Europe','North America','South America') NOT NULL DEFAULT 'Europe',
-  Region varchar
+  Country_Continent ENUM('Asia','Africa','Antarctica','Australia', 'Europe','North America','South America') NOT NULL DEFAULT 'Europe',
+  Country_Region varchar
 (25) NOT NULL DEFAULT '',
-  Capital int
+  Country_Capital int
 (11) NOT NULL DEFAULT 0,
-Population int
+Country_Population int
 (11) NOT NULL default 0,
   primary key
 (Country_Code)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-drop table city;
+
 create table city
 (
-  Id int(11) NOT NULL
-  auto_increment,
+  City_code int(11) NOT NULL
+  AUTO_INCREMENT,
   City_Name varchar
-  (30) NOT NULL DEFAULT '', 
+  (35) NOT NULL DEFAULT '', 
   Country_Code varchar
   (3) NOT NULL DEFAULT '',
-  District varchar
-  (20) NOT NULL DEFAULT '',
-  Population int
+  City_Population int
   (11) NOT NULL DEFAULT 0,
   primary key
-  (Id),
-  KEY Country_Code
-  (Country_Code),
-  CONSTRAINT city_ibfk_1 FOREIGN KEY
+  (City_code),
+  foreign key
   (Country_Code) REFERENCES country
   (Country_Code)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+  /*insert into country (Country_Code,Country_Name,Continent,Region,Capital, Population)  Values*/
   insert into country
-    (Country_Code,Country_Name,Continent,Region,Capital, Population)
-  Values
-    ('ABW', 'Aruba', 'North America', 'Caribbean', 0, 0),
-    ('ANT', 'Netherlands Antilles', 'North America', 'Caribbean', 0, 0),
-    ('ARE', 'United Arab Emirates', 'Asia', 'Middle East', 2, 0),
-    ('ARG', 'Argentina', 'South America', 'South America', 60, 3),
-    ('ARM', 'Armenia', 'Asia', 'Middle East', 80, 0),
-    ('USA', 'United States', 'North America', 'North America', 5, 8),
-    ('THA', 'Thailand', 'Asia', 'Southeast Asia', 9, 0),
-    ('SWE', 'Sweden', 'Europe', 'Nordic Countries', 8, 0),
-    ('DNK', 'Denmark', 'Europe', 'Nordic Countries', 2, 0),
-    ('PHL', 'Philippines', 'Asia', 'Southeast Asia', 3, 2),
-    ('IND', 'India', 'Asia', 'Southern and Central Asia', 5, 0),
-    ('ESP', 'Spain', 'Europe', 'Southern Europe', 5, 0),
-    ('TUR', 'Turkey', 'Asia', 'Middle East', 0, 0),
-    ('CHN', 'China', 'Asia', 'Eastern Asia', 0, 8),
-    ('PER', 'Peru', 'South America', 'South America', 0, 0);
+  value
+  ('ABW',
+  'Aruba',
+  'North America',
+  'Caribbean',
+  0,
+  0
+  );
+  insert into country
+  value
+  ('ANT',
+  'Netherlands Antilles',
+  'North America',
+  'Caribbean',
+  0,
+  0
+  );
+  insert into country
+  value
+  ('ARE',
+  'United Arab Emirates',
+  'Asia',
+  'Middle East',
+  2,
+  0
+  );
+  insert into country
+  value('ARG',
+  'Argentina',
+  'South America',
+  'South America',
+  60,
+  3
+  );
+  insert into country
+  value('ARM',
+  'Armenia',
+  'Asia',
+  'Middle East',
+  80,
+  0
+  );
+  insert into country
+  value
+  ('USA',
+  'United States',
+  'North America',
+  'North America',
+  5,
+  8
+  );
+  insert into country
+  value('THA',
+  'Thailand',
+  'Asia',
+  'Southeast Asia',
+  9,
+  0
+  );
+  insert into country
+  value('SWE',
+  'Sweden',
+  'Europe',
+  'Nordic Countries',
+  8,
+  0
+  );
+  insert into country
+  value('DNK',
+  'Denmark',
+  'Europe',
+  'Nordic Countries',
+  2,
+  0
+  );
+  insert into country
+  value('PHL',
+  'Philippines',
+  'Asia',
+  'Southeast Asia',
+  3,
+  2
+  );
+  insert into country
+  value('IND',
+  'India',
+  'Asia',
+  'Southern and Central Asia',
+  5,
+  0
+  );
+  insert into country
+  value('ESP',
+  'Spain',
+  'Europe',
+  'Southern Europe',
+  5,
+  0
+  );
+  insert into country
+  value('TUR',
+  'Turkey',
+  'Asia',
+  'Middle East',
+  0,
+  0
+  );
+  insert into country
+  value('CHN',
+  'China',
+  'Asia',
+  'Eastern Asia',
+  0,
+  8
+  );
+  insert into country
+  value('PER',
+  'Peru',
+  'South America',
+  'South America',
+  0,
+  0
+  );
 
 
   insert into city
-    (Id,City_Name,Country_Code,District,Population)
-  values
-    (3320, 'Bangkok', 'THA', 'Bangkok', 6320174),
-    (3321, 'Nonthaburi', 'THA', 'Nonthaburi', 292100),
-    (3322, 'Nakhon Ratchasima', 'THA', 'Nakhon Ratchasima', 181400),
-    (3323, 'Chiang Mai', 'THA', 'Chiang Mai', 171100),
-    (3324, 'Udon Thani', 'THA', 'Udon Thani', 158100),
-    (3325, 'Hat Yai', 'THA', 'Songkhla', 148632),
-    (3326, 'Khon Kaen', 'THA', 'Khon Kaen', 126500),
-    (3327, 'Pak Kret', 'THA', 'Nonthaburi', 126055),
-    (3328, 'Nakhon Sawan', 'THA', 'Nakhon Sawan', 123800),
-    (3329, 'Ubon Ratchathani', 'THA', 'Ubon Ratchathani', 116300),
-    (3330, 'Songkhla', 'THA', 'Songkhla', 94900),
-    (3331, 'Nakhon Pathom', 'THA', 'Nakhon Pathom', 94100),
-    (3048, 'Stockholm', 'SWE', 'Lisboa', 750348),
-    (3049, 'Gothenburg [GÃ¶teborg]', 'SWE', 'West GÃ¶tanmaan lÃ¤n', 466990),
-    (3050, 'MalmÃ¶', 'SWE', 'SkÃ¥ne lÃ¤n', 259579),
-    (3051, 'Uppsala', 'SWE', 'Uppsala lÃ¤n', 189569),
-    (3052, 'LinkÃ¶ping', 'SWE', 'East GÃ¶tanmaan lÃ¤n', 133168),
-    (3053, 'VÃ¤sterÃ¥s', 'SWE', 'VÃ¤stmanlands lÃ¤n', 126328),
-    (3054, 'Ã–rebro', 'SWE', 'Ã–rebros lÃ¤n', 124207),
-    (3055, 'NorrkÃ¶ping', 'SWE', 'East GÃ¶tanmaan lÃ¤n', 122199),
-    (3056, 'Helsingborg', 'SWE', 'SkÃ¥ne lÃ¤n', 117737),
-    (3057, 'JÃ¶nkÃ¶ping', 'SWE', 'JÃ¶nkÃ¶pings lÃ¤n', 117095),
-    (3058, 'UmeÃ¥', 'SWE', 'VÃ¤sterbottens lÃ¤n', 104512),
-    (3059, 'Lund', 'SWE', 'SkÃ¥ne lÃ¤n', 98948),
-    (3060, 'BorÃ¥s', 'SWE', 'West GÃ¶tanmaan lÃ¤n', 96883),
-    (3061, 'Sundsvall', 'SWE', 'VÃ¤sternorrlands lÃ¤', 93126),
-    (3062, 'GÃ¤vle', 'SWE', 'GÃ¤vleborgs lÃ¤n', 90742);
+  value
+  (1,
+  'Bangkok',
+  'THA',
+  6320174
+  );
+  /*(City_code,City_Name,Country_Code,City_Population)*/
+  (2, 'Nonthaburi', 'THA', 292100),
+  (3, 'Nakhon Ratchasima', 'THA', 181400),
+  (4, 'Chiang Mai', 'THA', 171100),
+  (5, 'Udon Thani', 'THA', 158100),
+  (6, 'Hat Yai', 'THA', 148632),
+  (7, 'Khon Kaen', 'THA', 126500),
+  (8, 'Pak Kret', 'THA', 126055),
+  (9, 'Nakhon Sawan', 'THA', 123800),
+  (10, 'Ubon Ratchathani', 'THA', 116300),
+  (11, 'Songkhla', 'THA', 94900),
+  (12, 'Nakhon Pathom', 'THA', 94100),
+  (13, 'Stockholm', 'SWE', 750348),
+  (14, 'Gothenburg [GÃ¶teborg]', 'SWE', 466990),
+  (15, 'MalmÃ¶', 'SWE', 259579),
+  (16, 'Uppsala', 'SWE', 189569),
+  (17, 'LinkÃ¶ping', 'SWE', 133168),
+  (18, 'VÃ¤sterÃ¥s', 'SWE', 126328),
+  (19, 'Ã–rebro', 'SWE', 124207),
+  (20, 'NorrkÃ¶ping', 'SWE', 122199),
+  (21, 'Helsingborg', 'SWE', 117737),
+  (22, 'JÃ¶nkÃ¶ping', 'SWE', 117095),
+  (23, 'UmeÃ¥', 'SWE', 104512),
+  (24, 'Lund', 'SWE', 98948),
+  (25, 'BorÃ¥s', 'SWE', 96883),
+  (26, 'Sundsvall', 'SWE', 93126),
+  (27, 'GÃ¤vle', 'SWE', 90742);
