3d42d78e4f3880a98fc32bac2f680eaf45faa129
Suthatta Lindstrii <psuthatta@gmail.com>
2019-10-10 08:15:56 +0200

    changed data structure

--- database/week1/world.sql
+++ database/week1/world.sql
@@ -6,11 +6,10 @@ show tables;
 drop table if exists country;
 create table country
 (
-  Country_Code varchar(3) NOT NULL DEFAULT '',
+  Country_Code int(3) NOT NULL AUTO_INCREMENT,
   Country_Name varchar(30) NOT NULL DEFAULT '',
   Country_Continent ENUM('Asia','Europe','NorthAmerica','SouthAmerica') NOT NULL DEFAULT 'Europe',
-  Country_Region varchar
-(25) NOT NULL DEFAULT '',
+  Country_SurfaceArea float(10,2) NOT NULL DEFAULT '0.00',
   Country_Population int
 (11) NOT NULL default 0,
   primary key
@@ -22,55 +21,38 @@ show tables;
 drop table if exists city;
 create table city
 (
-  City_Code int(11) NOT NULL
-  AUTO_INCREMENT,
-  City_Name varchar
-  (35) NOT NULL DEFAULT '', 
-  Country_Code varchar
-  (3) NOT NULL DEFAULT '',
-  City_Population int
-  (11) NOT NULL DEFAULT 0,
-  primary key
-  (City_Code),
-  foreign key
-  (Country_Code) REFERENCES country
-  (Country_Code)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  City_Code int(3) NOT NULL unique,
+  City_Name varchar (30) NOT NULL DEFAULT '', 
+  Country_Code int(3) NOT NULL AUTO_INCREMENT,
+  City_Population int (11) NOT NULL DEFAULT 0,
+  primary key  (City_Code),
+  foreign key(Country_Code) REFERENCES country
+  (Country_Code) )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 show tables;
   /*insert into country (Country_Code,Country_Name,Continent,Region,Capital, Population)  Values*/
   insert into country
-  value('001','Singapo','Asia','Caribbean',
-  0
-  );
-  insert into country
-  value
-  ('NDL',
-  'Netherlands',
-  'Europe',
-  'Caribbean',
-  0
-  );
+  value('001','Singapo','Asia',110, 5  );
   insert into country
   value
   ('002',
   'India',
   'Asia',
-  'Middle East',
-  0
+  10.5,
+  5
   );
   insert into country
   value('003',
   'Argentina',
   'SouthAmerica',
-  'SouthAmerica',
+  4.5,
   3
   );
   insert into country
   value('004',
   'Turkey',
   'Europe',
-  'Middle East',
+  0,
   0
   );
   insert into country
@@ -78,78 +60,77 @@ show tables;
   ('005',
   'United States',
   'NorthAmerica',
-  'NorthAmerica',
+  0,
   8
   );
   insert into country
   value('006',
   'Thailand',
   'Asia',
-  'Asia',
+  0,
   0
   );
   insert into country
   value('007',
   'Sweden',
   'Europe',
-  'NordicCountries',
-  0
+  21.5,
+  8
   );
   insert into country
   value('008',
   'Denmark',
   'Europe',
-  'NordicCountries',
-  0
+ 10,
+  800
   );
   insert into country
   value('009',
   'Philippines',
   'Asia',
-  'SoutheastAsia',
+  9,
   2
   );
   insert into country
   value('010',
   'Los',
   'Asia',
-  'SouthernAsia',
-  0
+  40,
+  82
   );
   insert into country
   value('011',
   'Spain',
   'Europe',
-  'Southern Europe',
-  0
+ 6,
+  7
   );
   insert into country
   value('012',
   'Cannada',
   'SouthAmerica',
-  'MiddleEast',
-  0
+  80,
+  4
   );
   insert into country
   value('013',
   'China',
   'Asia',
-  'EasternAsia',
+  90,
   8
   );
   insert into country
   value('014',
   'Peru',
   'SouthAmerica',
-  'SouthAmerica',
-  0
+  100,
+  30
   );
 
   select count(*) as TotalCountries
   from country;
 
   /*insert into city   values (City_code,City_Name,Country_Code,City_Population)*/
-  <<<<<<< HEAD
   insert into city
   value
   (1,
